d1a7e16d5780abcb88a6cb22b4bf28b3a502aa79
Christian Fischer <christian@chfi.se>
2016-11-08 13:58:53 -0600

    Clean up Purescript stuff

--- purs/QtlSourcePurs.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-// exports.SourceAdapters = require('../js/sourceadapters.js');
-// exports.DAS = require('../js/das.js');
-exports.das = require('../../ffitest.js').butt;
--- purs/QtlSourcePurs.purs
+++
@@ -1,6 +0,0 @@
-module QtlSourcePurs where
-
-foreign import das :: String
-
-qtl :: String
-qtl = das
